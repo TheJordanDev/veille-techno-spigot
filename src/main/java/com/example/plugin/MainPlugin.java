48a8501bf7845f0705784f06cccd444ac8932b6d
TheJordanDev <thejordan.dev@gmail.com>
2022-12-12 11:43:16 +0100

    Template

---
+++ src/main/java/com/example/plugin/MainPlugin.java
@@ -0,0 +1,17 @@
+package com.example.plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MainPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+}
